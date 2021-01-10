a4741a24df6c0887618f5bc1d94e57d0963aece3
Matheus Cardoso <matheussp.cardoso@gmail.com>
2021-01-10 15:40:14 -0300

    Add Routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,9 @@
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
-import Footer from './Footer';
+import Routes from './Routes';
 
 function App() {
   return (
-        <>
-        <Navbar/>
-        <Home/>
-    </>
+    <Routes/>
   );
 }
 
--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,7 +1,7 @@
 import './styles.css';
-import {ReactComponent as YoutubeIcon} from './youtube.svg';
-import {ReactComponent as LinkedinIcon} from './linkedin.svg';
-import {ReactComponent as InstagramIcon} from './instagram.svg';
+import { ReactComponent as YoutubeIcon } from './youtube.svg';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
 
 function Footer() {
     return (
@@ -9,10 +9,14 @@ function Footer() {
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
             <div className="footer-icons">
                 <a href="https://www.youtube.com/c/DevSuperior" target="_new">
-                    <YoutubeIcon/>
-                    </a>                
-                <a href="https://www.linkedin.com/school/devsuperior/" target="_new"><LinkedinIcon/></a>
-                <a href="https://www.instagram.com/devsuperior.ig/" target="_new"><InstagramIcon/></a>
+                    <YoutubeIcon />
+                </a>
+                <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
+                    <LinkedinIcon />
+                </a>
+                <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
+                    <InstagramIcon />
+                </a>
             </div>
         </footer>
     )
