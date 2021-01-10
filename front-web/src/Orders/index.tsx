c5717c609c8a1910c7cc2e59fadfab898a6b48f2
Matheus Cardoso <matheussp.cardoso@gmail.com>
2021-01-10 16:54:20 -0300

    Add Orders and products from api

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
+import { Product } from './types';
 import './styles.css';
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    }, [])
+
     return (
         <>
             <div className="orders-container">
                 <StepsHeader />
+                <ProductsList products={products}/>                
             </div>
         </>
     )
